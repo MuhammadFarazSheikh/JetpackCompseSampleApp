9d050a98fd286a433178f4fc4afa205cf77f6452
Muhammad Faraz Sheikh <faraz.sheikh@prvidr.com>
2022-06-01 15:27:50 +0500

    Code Push

--- app/src/main/java/com/lovetocode/diseasesymptoms/composeclasses/ToDoFirstScreen.kt
+++ app/src/main/java/com/lovetocode/diseasesymptoms/composeclasses/ToDoFirstScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.runtime.Composable
@@ -13,7 +14,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.TextStyle
@@ -27,19 +27,9 @@ import androidx.constraintlayout.compose.ConstraintLayout
 fun userLogin()
 {
     var textField by remember { mutableStateOf("faraz") }
-    val (userNameField) = createRefs()
     ConstraintLayout(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
-        BasicTextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-                .background(color = Color.Red)
-                .border(2.dp, color = Color.Green, shape = RectangleShape),
-            value = textField,
-            onValueChange = {
-                textField = it
-            }, singleLine = true
-        )
+        val (userNameField) = createRefs()
+
 
     }
 }
